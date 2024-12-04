83c635014b535a1c7834ada452a3724f3ef5e11f
typelias <98ille@gmail.com>
2024-12-04 18:18:13 +0100

    Removed insert mode from movement remapping
    This was a stupid mistake...

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -4,10 +4,10 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Better movment
-vim.keymap.set({ 'n', 'v', 'i', 'c', 'o', 's' }, 'H', '^')
-vim.keymap.set({ 'n', 'v', 'i', 'c', 'o', 's' }, 'L', '$')
-vim.keymap.set({ 'n', 'v', 'i', 'c', 'o', 's' }, 'J', '}')
-vim.keymap.set({ 'n', 'v', 'i', 'c', 'o', 's' }, 'K', '{')
+vim.keymap.set({ 'n', 'v', 'c', 'o', 's' }, 'H', '^')
+vim.keymap.set({ 'n', 'v', 'c', 'o', 's' }, 'L', '$')
+vim.keymap.set({ 'n', 'v', 'c', 'o', 's' }, 'J', '}')
+vim.keymap.set({ 'n', 'v', 'c', 'o', 's' }, 'K', '{')
 
 vim.keymap.set('o', 'H', '^')
 vim.keymap.set('o', 'L', '$')
